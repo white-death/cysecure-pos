99ec943f5cf4b38f65d9fa25e94573102a10ae89
white-death <tester.whitedeath@gmail.com>
2026-05-06 18:17:12 +0530

    Wave - 2 Started

--- pos-backend/app/core/config.py
+++ pos-backend/app/core/config.py
@@ -1,30 +1,32 @@
-from pydantic_settings import BaseSettings
 from urllib.parse import quote_plus
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
 
     KEYCLOAK_URL: str
     KEYCLOAK_REALM: str
-
     KEYCLOAK_ADMIN_USERNAME: str
     KEYCLOAK_ADMIN_PASSWORD: str
     KEYCLOAK_ADMIN_CLIENT_ID: str
 
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
 
 
 settings = Settings()
 
-encoded_password = quote_plus(settings.DB_PASS)
+encoded_password = quote_plus(settings.POSTGRES_PASSWORD)
 
 DATABASE_URL = (
-    f"postgresql://{settings.DB_USER}:{encoded_password}"
-    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+    f"postgresql://{settings.POSTGRES_USER}:"
+    f"{encoded_password}@"
+    f"{settings.POSTGRES_HOST}:"
+    f"{settings.POSTGRES_PORT}/"
+    f"{settings.POSTGRES_DB}"
 )
